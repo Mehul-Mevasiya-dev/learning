62bbbb7d4ddd2b7b71bb1a880957cd4fbec9c654
Mehul Mevasiya <mehulmevasiya005@gmail.com>
2025-11-11 23:57:03 +0530

    day-4 completed

---
+++ day-4/script.js
@@ -0,0 +1,38 @@
+
+alert("Welcome to Day 4!");
+
+
+let name = "Mehul";
+let age = 20
+console.log("Name:", name);
+console.log("Age:", age)
+
+let userName = prompt("What is your name?");
+alert("Hello, " + userName + "! Welcome to JavaScript learning!");
+
+let num1 = 5;
+let num2 = 30;
+let sum = num1 + num2;
+alert("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+ 
+
+console.log("Welcome to the console version!");
+
+
+let string = "I love coding!";
+let number = 42;
+let boolean = true;
+
+console.log("String:", string);
+console.log("Number:", number);
+console.log("Boolean:", boolean);
+
+
+let numberToCheck = 15;
+
+if (numberToCheck > 10) {
+  console.log(numberToCheck + " is greater than 10.");
+} else {
+  console.log(numberToCheck + " is not greater than 10.");
+}
